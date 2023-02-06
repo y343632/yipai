5c47c28389d32e2d06c4b35a1b8cb42b81680811
y343632 <117623833+y343632@users.noreply.github.com>
2023-02-06 17:48:26 +0800

    Add files via upload

--- news/News.jsx
+++ news/News.jsx
@@ -1,25 +1,128 @@
 // import React from 'react'
-import React, { useState } from 'react'
 import './news.css'
 import { Link } from 'react-router-dom'
-import news__banner from '../../assets/news__banner.png'
 import Button from 'react-bootstrap/Button'
 import { TbArrowsSort } from 'react-icons/tb'
 import { BiTimeFive } from 'react-icons/bi'
-import axios from 'axios'
-import { useEffect } from 'react'
+import { MdArrowForwardIos,MdArrowBackIosNew } from 'react-icons/md'
+import { RiArrowUpSLine,RiArrowDownSLine } from 'react-icons/ri'
+import axios from 'axios';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
-const News = () => {
-  const [news, setNews] = useState([])
+
+function News  ()  {
+  window.scrollTo(0, 0);
+  // 為了處理網址
+  let navigate = useNavigate();
+  const { currentPage } = useParams();
+  const [page, setPage] = useState(parseInt(currentPage, 10) || 1); // 目前在哪一頁
+  const [totalPage, setTotalPage] = useState(); // 總共有幾頁
+
+  const [news, setNews] = useState([]);
+
+  useEffect(()=>{
+    console.log('空陣列的 useEffect');
+  },[]);
+
   useEffect(() => {
+    console.log('第二個參數是空陣列');
+    // 在 component 初始化的時候跑一次
+    // 通常會把去跟後端要資料的動作放在這裡
     async function getNews() {
-      let response = await axios.get('http://localhost:3001/news')
-      setNews(response.data)
+      let response = await axios.get(`http://localhost:3001/news?page=${page}`);
+      setNews(response.data.data);
+      console.log(response.data.data);
+      setTotalPage(response.data.pagination.totalPage);
     }
-    getNews()
-  }, [])
+    getNews();
+  }, [page]);
 
-  // const [toggleMenu, setToggleMenu] = useState(false)
+  
+  const getPages = () => {
+    let pages = [];
+  
+    // 顯示「上一頁」
+    if (page > 1) {
+      pages.push(
+        <RiArrowUpSLine
+          style={{
+            display: 'inline-block',
+            margin: '2px',
+            borderColor: '#dbdbdb',
+            borderWidth: '1px',
+            width: '50px',
+            height: '50px',
+            borderRadius: '3px',
+            textAlign: 'center',
+            
+          }}
+          key={-1}
+          onClick={(e) => {
+            setPage(page - 1);
+            navigate(`/news?page=${page - 1}`);
+          }}
+        >
+          {'<'}
+        </RiArrowUpSLine>
+      );
+    }
+  
+    // 顯示頁碼
+    // for (let i = 1; i <= 5; i++) {
+    //   pages.push(
+    //     <li
+    //       style={{
+    //         display: 'inline-block',
+    //         margin: '2px',
+    //         backgroundColor: 'black',
+    //         borderColor: page === i ? '#00d1b2' : '#dbdbdb',
+    //         color: page === i ? '#fff' : '#363636',
+    //         borderWidth: '1px',
+    //         width: '28px',
+    //         height: '28px',
+    //         borderRadius: '3px',
+    //         textAlign: 'center',
+    //       }}
+    //       key={i}
+    //       onClick={(e) => {
+    //         setPage(i);
+    //         navigate(`/news?page=${i}`);
+    //       }}
+    //     >
+    //       {i}
+    //     </li>
+    //   );
+    // }
+  
+    // 顯示「下一頁」
+    if (page < totalPage) {
+      pages.push(
+        <RiArrowDownSLine
+          style={{
+            display: 'flex',
+            margin: '2px',
+            borderColor: '#dbdbdb',
+            borderWidth: '1px',
+            width: '50px',
+            height: '50px',
+            borderRadius: '3px',
+            textAlign: 'center',
+            
+          }}
+          key={-2}
+          onClick={(e) => {
+            setPage(page + 1);
+            navigate(`/news?page=${page + 1}`);
+          }}
+        >
+          
+        </RiArrowDownSLine>
+      );
+    }
+  
+    return pages;
+  };
 
   return (
     <div>
@@ -50,24 +153,26 @@ const News = () => {
         </nav>
         <main className="News__main">
           <div className="News__main-wrap">
-            {news.map((news, index) => {
+            {news.map((news_data, index) => {
               return (
-                <div key={news.news_id} classMame="News__img-id-wrap">
-                  <Link to={`/news/${news.news_id}`}>
-                    <img className="News__card-img-top" src={news.img_file} />
-                    <h4 className="News__newsId ">{news.name}</h4>
+                <div key={news_data.news_id} classMame="News__img-id-wrap">
+                  <Link to={`/news/${news_data.news_id}`}>
+                    <img className="News__card-img-top" src={news_data.img_file} />
+                    <h4 className="News__newsId ">{news_data.name}</h4>
                     <p className="News__article">
                       <BiTimeFive />
-                      {news.news_time}
+                      {news_data.news_time}
                     </p>
-                    <p className="News__text">{news.news_introduction}</p>
+                    <p className="News__text">{news_data.news_introduction}</p>
                   </Link>
                 </div>
               )
             })}
           </div>
         </main>
+        <ul>{getPages()}</ul>
       </section>
+     
     </div>
   )
 }
